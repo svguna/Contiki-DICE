be201ee0308e317f0caeed56717893cecd0be852
Stefan Guna <svguna@gmail.com>
2012-05-12 12:48:54 +0300

    added dummy group monitor

---
+++ apps/dice/dummy_group.c
@@ -0,0 +1,29 @@
+#include "group.h"
+
+int groupmon_init(uint32_t newNeighborLatency, uint32_t missingNeighborLatency)
+{
+    return 0;
+}
+
+
+int groupmon_stop()
+{
+    return 0;
+}
+
+
+int groupmon_reset()
+{
+    return 0;
+}
+
+
+void groupmon_forceupdate(rimeaddr_t *addr)
+{
+}
+
+
+int groupmon_isalive(rimeaddr_t *addr)
+{
+    return 1;
+}
